15a60aa709521059071e9578cb25efde06f4eb17
Jacob Brady <polyfish42@gmail.com>
2018-06-02 15:14:25 -0400

    First passing parser test

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,13 @@
 module Main exposing (..)
 
+import Combine exposing (parse)
 
-parse : String -> String
-parse text =
-    "I'm just here to say what works for me"
+
+parseTranscriptionXML : String -> Result String String
+parseTranscriptionXML transcription =
+    case parse (Combine.string "hello") transcription of
+        Ok ( _, stream, result ) ->
+            Ok result
+
+        Err ( _, stream, errors ) ->
+            Err (String.join " or " errors)
--- tests/MainTest.elm
+++ tests/MainTest.elm
@@ -13,11 +13,14 @@ suite =
             [ test "parses on paragraph" <|
                 \_ ->
                     let
-                        transcriptXML =
+                        transcription =
                             """
                         <p t="0" d="5009" w="1"><s p="2" ac="68">I&#39;m</s><s t="510" ac="252"> just</s><s t="1410" ac="252"> here</s><s p="1" t="1589" ac="219"> to</s><s t="1709" ac="252"> say</s><s t="1920" ac="252"> what</s><s t="2220" ac="240"> works</s><s p="2" t="2700" ac="124"> for</s><s p="2" t="3030" ac="165"> me</s></p>
                         """
+
+                        hello =
+                            "hello"
                     in
-                    Expect.equal (Main.parse transcriptXML) "I'm just here to say what works for me"
+                    Expect.equal (Main.parseTranscriptionXML hello) (Ok "hello")
             ]
         ]
